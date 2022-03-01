6263bc7d69500a0786ebc125a64ebdde8be0300e
Hutchi299 <rhys.hutchinson299@gmail.com>
2022-03-01 23:17:13 +1000

    deployment branch init

--- src/js/model/model.js
+++ src/js/model/model.js
@@ -1,20 +1,20 @@
-import iconSedan from '../../images/icon-sedans.svg';
-import iconSUV from '../../images/icon-suvs.svg';
-import iconLuxury from '../../images/icon-luxury.svg';
+// import iconSedan from '../../images/icon-sedans.svg';
+// import iconSUV from '../../images/icon-suvs.svg';
+// import iconLuxury from '../../images/icon-luxury.svg';
 
 export const data = [
     {
-        icon: iconSedan,
+        icon: './src/images/icon-sedans.svg',
         title: 'Sedans',
         content: 'Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.'
     },
     {
-        icon: iconSUV,
+        icon: './src/images/icon-suvs.svg',
         title: 'SUVs',
         content: 'Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.'
     },
     {
-        icon: iconLuxury,
+        icon: './src/images/icon-luxury.svg',
         title: 'Luxury',
         content: 'Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.'
     }
